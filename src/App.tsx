c5caf8d9ec2f21d73cdc34ce43074308f987c4d8
adamhinton <adam.d.hinton@gmail.com>
2022-05-19 22:27:08 -0400

    render TDL in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import TodoList from "./components/TodoList";
 // Now this has to return a functional component
 // would fail without a return stmt frex
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 };
 
 export default App;
