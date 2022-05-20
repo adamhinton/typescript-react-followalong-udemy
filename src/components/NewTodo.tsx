a8207238e0804314cedc5b1975730e8dafd3bcc4
adamhinton <adam.d.hinton@gmail.com>
2022-05-19 22:38:52 -0400

    submit btn

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -5,6 +5,7 @@ const NewTodo = () => {
         <label htmlFor="todo-text">Todo Text</label>
         <input type="text" id="todo-text" />
       </div>
+      <button type="submit">ADD TODO</button>
     </form>
   );
 };
