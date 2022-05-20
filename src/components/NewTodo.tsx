18a043637d4fe171da6af460eb0b74f8bb65e6e8
adamhinton <adam.d.hinton@gmail.com>
2022-05-19 22:38:29 -0400

    input and label

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,5 +1,12 @@
 const NewTodo = () => {
-  return <form></form>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" />
+      </div>
+    </form>
+  );
 };
 
 export default NewTodo;
