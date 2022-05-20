c9872414fde974fd8e5c064bbb6a7dbfbe8a489b
adamhinton <adam.d.hinton@gmail.com>
2022-05-19 22:26:26 -0400

    basic TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const TodoList: React.FC = () => {
-  return <ul></ul>;
+  const todos = [{ id: "t1", text: "finish the course" }];
+
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return <li key={todo.id}>{todo.text}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default TodoList;
