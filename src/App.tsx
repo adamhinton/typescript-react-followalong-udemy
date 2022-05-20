b36063381c41738b4b422463be313ff3bc05232b
adamhinton <adam.d.hinton@gmail.com>
2022-05-19 22:19:04 -0400

    more FC notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 // Now this has to return a functional component
+// would fail without a return stmt frex
 const App: React.FC = () => {
   return <div className="App"></div>;
 };
