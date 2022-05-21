49ccb983d488ad919bbb479f7099bcee579bee31
adamhinton <adam.d.hinton@gmail.com>
2022-05-20 23:17:53 -0400

    teach useState to expect an array of Todo objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./components/todo.model";
 
 // Now this has to return a functional component
 // would fail without a return stmt frex
 const App: React.FC = () => {
   // telling TS to expect an array of these objects
-  const [todos, setTodos] = useState<{ id: string; text: string }[]>([]);
+  // Note we're telling it to expect an array of Todo model imported from todo.model.ts
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
     setTodos([{ id: Math.random().toString(), text: text }]);
