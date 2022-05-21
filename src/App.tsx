0bf85549108597efa9543b40fdb469247f1b7241
adamhinton <adam.d.hinton@gmail.com>
2022-05-20 23:03:33 -0400

    dummy todoAddHandler fxn

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import NewTodo from "./components/NewTodo";
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "finish the course" }];
 
+  const todoAddHandler = (text: string) => {
+    console.log("text:", text);
+  };
+
   return (
     <div className="App">
       <NewTodo />
